734f4f7c945a3f9af889ad87bbf8869e7e1888b2
zabidenhtf <policyaskrimerov@gmail.com>
2026-03-20 11:45:31 +0000

    Added configuration structures

---
+++ Sources/Settings.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2026 Mykyta Polishyk
+// This project is licensed under the MIT License.
+// See the LICENSE file for details.
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <boost/asio.hpp>
+
+// Client configuration
+struct ClientSettings{
+	std::string Username;
+	boost::asio::ip::address_v4 MasterServerAddr;
+	bool DarkTheme;
+};
+
+// Server configuration
+struct ServerSettings{
+	std::string Title;
+	bool UseCustomVideos;
+	bool ChatFilter;
+	bool IsPrivate;
+	std::string Password;
+	std::vector<std::string> BannedWords;
+	std::string CustomVideosDirectory;
+	std::string GoogleAPIKey;
+};
